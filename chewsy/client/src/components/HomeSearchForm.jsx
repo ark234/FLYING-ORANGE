45b8c1ca552caf4ee9e4cbd7e11ca16d7e10deb6
raksheen <raksheenayaz@gmail.com>
2018-02-17 16:48:10 -0500

    created react comp  files

---
+++ chewsy/client/src/components/HomeSearchForm.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+
+class HomeSearchForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return 
+    <div>
+    <button>Log In</button>
+    <button>Sign Up</button>
+    </div>
+    
+    <Form>
+    <h2>Allergens</h2>
+    <h2>Cravings</h2>
+    <button>Go</button>
+    </Form>
+  }
+}
+
+export default HomeSearchForm;
+
---
+++ chewsy/client/src/components/RecipeInfo.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+
+class RecipeInfo extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return 
+    <h1>Recipe Information</h1>;
+    <h3>Image</h3>
+    <h3>Health Labels</h3>
+    <h3>Nutrtion Label</h3>
+    <h3>Ingredients</h3>
+    <button>Save</button>
+    <button>Recipe: iFrame Modal will Pop Up</button>
+  }
+}
+
+export default RecipeInfo;
