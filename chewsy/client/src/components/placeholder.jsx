955535451ef14af93cf242434e09804c01a4ec01
raksheen <raksheenayaz@gmail.com>
2018-02-17 16:08:02 -0500

    roxy: created homepage component

--- chewsy/client/src/components/placeholder.jsx
+++ chewsy/client/src/components/placeholder.jsx
@@ -1,1 +1,14 @@
-// placeholder
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class HomeSearchForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1>Home Search Form</h1>;
+  }
+}
+
+export default HomeSearchForm;
