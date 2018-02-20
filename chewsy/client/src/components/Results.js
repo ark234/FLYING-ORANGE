f5cca38539159ace0e95515255bc2ca86acdc88e
Timothy Lowe <timothylowe247@gmail.com>
2018-02-19 20:53:31 -0500

    post reqest on results.js debugged

--- chewsy/client/src/components/Results.js
+++ chewsy/client/src/components/Results.js
@@ -1,8 +1,6 @@
-
 // import { Link } from "react-router-dom";
-import React, { Component } from 'react';
-import axios from 'axios';
-
+import React, { Component } from "react";
+import axios from "axios";
 
 class ShowResults extends Component {
   constructor(props) {
@@ -14,37 +12,29 @@ class ShowResults extends Component {
     axios({
       url: "http://localhost:8080/recipes/moreInfo",
       method: "post",
-      data: uri
+      data: {uri}
     }).then(response => {
       console.log("POST FOR MORE INFO", response);
       this.props.history.push("/moreInfo");
     });
   }
 
-  
-
-
-
   render() {
     const results = this.props.results;
-        const resultsList = results.hits.map(recipeObject => {
-          const recipeInfo = recipeObject.recipe;
+    const resultsList = results.hits.map(recipeObject => {
+      const recipeInfo = recipeObject.recipe;
 
-          return (
-            <div
-              key={recipeInfo.uri}
-              onClick={() => this.moreInfo(recipeInfo.uri)}
-            >
-              <h2>{recipeInfo.label}</h2>
-              <h6>{recipeInfo.healthLabels}</h6>
-              <img src={recipeInfo.image} width="100px" height="100px" />
-            </div>
-          )
-        })
+      return (
+        <div key={recipeInfo.uri} onClick={() => this.moreInfo(recipeInfo.uri)}>
+          <h2>{recipeInfo.label}</h2>
+          <h6>{recipeInfo.healthLabels}</h6>
+          <img src={recipeInfo.image} width="100px" height="100px" />
+        </div>
+      );
+    });
 
-    return (<div>{resultsList}</div>);
-}
-
+    return <div>{resultsList}</div>;
+  }
 }
 
 export default ShowResults;
