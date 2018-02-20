0d9135eaf9c5c8c97e7abbb905ebd016b480e852
ark <ark234@gmail.com>
2018-02-19 23:30:28 -0500

    renamed routes to /login and /register

--- chewsy/controllers/users.js
+++ chewsy/controllers/users.js
@@ -15,7 +15,6 @@
 //                                             //
 /////////////////////////////////////////////////
 
-
 const router = require('express').Router();
 
 const usersModel = require('../models/users.js');
@@ -25,9 +24,9 @@ const usersModel = require('../models/users.js');
 // const passport = require('passport');
 // const auth = require('../services/auth');
 /////////////////////////////////////////////////
-// This has not been done in index.js... 
+// This has not been done in index.js...
 // Do we need it here?
-// 
+//
 // router.get('/', (req, res, next) => {
 //     res.redirect('users/profile');
 // });
@@ -37,71 +36,62 @@ console.log('Loading controllers/users...');
 
 // TODO: define GET request for '/' to retrieve all users...
 router.get('/', usersModel.getAllUsers, (req, res, next) => {
+	console.log('In router.get, usersModel.getAllUsers...');
 
-  console.log('In router.get, usersModel.getAllUsers...');
-
-  res.json(res.locals.allUsers);
-
+	res.json(res.locals.allUsers);
 });
 
 // TODO: define GET request for '/:id' to retrieve user...
-router.get("/:id", usersModel.getUser, (req, res) => {
+router.get('/:id', usersModel.getUser, (req, res) => {
+	console.log('In router.get, usersModel.getUser...');
+	// const user = res.locals;
+	// console.log('User data: ', user);
+	console.log('User data: ', res.locals.user);
 
-  console.log('In router.get, usersModel.getUser...');
-  // const user = res.locals;
-  // console.log('User data: ', user);
-  console.log("User data: ", res.locals.user);
-
-  res.json(res.locals.user);
+	res.json(res.locals.user);
 });
 
 // TODO: define POST request for '/' to create user...
-router.post('/', usersModel.create, (req, res, next) => {
+router.post('/register', usersModel.create, (req, res, next) => {
+	// This route is supposed to handle sign up for
+	// a new user...
 
-  // This route is supposed to handle sign up for
-  // a new user...
+	console.log('In router.post, usersModel.create...');
 
-  console.log('In router.post, usersModel.create...');
-    
-    console.log(res.locals.newUserId);
+	console.log(res.locals.newUserId);
 
-    res.json(res.locals.newUserId);
+	res.json(res.locals.newUserId);
 });
 
-router.post('/', usersModel.updateLogin, (req, res, next) => {
+router.post('/login', usersModel.updateLogin, (req, res, next) => {
+	// This route is supposed to handle login for
+	// a user...
 
-  // This route is supposed to handle login for
-  // a user...
+	// Expected that entered email and password would
+	// be compared against DB table "users", and then
+	// "counter" increased to 1 to indicate active
+	// session...
 
-  // Expected that entered email and password would
-  // be compared against DB table "users", and then
-  // "counter" increased to 1 to indicate active 
-  // session...
+	console.log('In router.post, usersModel.create...');
 
-  console.log('In router.post, usersModel.create...');
-    
-    console.log(res.locals.newUserId);
+	console.log(res.locals.newUserId);
 
-    res.json(res.locals.newUserId);
+	res.json(res.locals.newUserId);
 });
 
 // TODO: define PUT request for '/:id' to update user...
 router.put('/:id', usersModel.update, (req, res) => {
+	console.log('In router.put, usersModel.update...');
 
-  console.log('In router.put, usersModel.update...');
-
-  console.log('res.locals: ', res.locals);
-  res.json(res.locals.userId);
+	console.log('res.locals: ', res.locals);
+	res.json(res.locals.userId);
 });
 
 // TODO: define DELETE request for '/:id' to delete user...
 router.delete('/:id', usersModel.destroy, (req, res) => {
+	console.log('In router.delete, usersModel.destroy...');
 
-  console.log('In router.delete, usersModel.destroy...');
-  
-  res.json({});
-
+	res.json({});
 });
-
 
 module.exports = router;
--- chewsy/models/users.js
+++ chewsy/models/users.js
@@ -15,7 +15,6 @@
 //                                             //
 /////////////////////////////////////////////////
 
-
 const db = require('../db/index.js');
 const axios = require('axios');
 const moment = require('moment');
@@ -29,181 +28,156 @@ const usersModel = {};
 // TODO: add method for retrieving users...
 
 usersModel.getAllUsers = (req, res, next) => {
+	console.log('in usersModel.getAllUsers...');
 
-  console.log('in usersModel.getAllUsers...');
-
-  db
-    .manyOrNone('SELECT * FROM users')
-    .then(data => {
-      res.locals.allUsers = data;
-      next();
-    })
-    .catch(error => {
-      console.log('Error: in usersModel.getAllUsers. Details: ', error);
-      next(error);
-    });
+	db
+		.manyOrNone('SELECT * FROM users')
+		.then(data => {
+			res.locals.allUsers = data;
+			next();
+		})
+		.catch(error => {
+			console.log('Error: in usersModel.getAllUsers. Details: ', error);
+			next(error);
+		});
 };
 
 usersModel.updateLogin = (req, res, next) => {
+	console.log('In usersModel.updateLogin...');
 
-  console.log('In usersModel.updateLogin...');
+	console.log('req.body:', req.body);
 
-  console.log('req.body:', req.body);
+	let { email, password_digest } = req.body;
 
-  let { email, password_digest } = req.body;
+	const passwDigest = bcrypt.hashSync(req.body.password_digest, 10);
 
-  const passwDigest = bcrypt.hashSync(req.body.password_digest, 10);
+	db
+		.one('UPDATE users SET counter=$1 WHERE email=$2 AND password_digest=$3 RETURNING id;', [
+			1,
+			email,
+			passwDigest
+		])
+		.then(userId => {
+			res.locals.userId = userId;
+			next();
+		})
+		.catch(error => {
+			// There supposed to be an arror for non-unique email...
 
-  db
-    .one('UPDATE users SET counter=$1 WHERE email=$2 AND password_digest=$3 RETURNING id;', 
-    [
-
-      1,
-      email,
-      passwDigest
-
-    ])
-    .then(userId => {
-      res.locals.userId = userId;
-      next();
-    })
-    .catch(error => {
-
-      // There supposed to be an arror for non-unique email...
-      
-      console.log('Error: in usersModel.update. Details: ', error);
-      next(error);
-    });
+			console.log('Error: in usersModel.update. Details: ', error);
+			next(error);
+		});
 };
-
 
 // TODO: add method for creating user...
 
 usersModel.create = (req, res, next) => {
+	console.log('In usersModel.create...');
 
-  console.log('In usersModel.create...');
+	// This method is supposed to handle sign up for
+	// a new user...
 
-  // This method is supposed to handle sign up for
-  // a new user...
+	// This is where we obtain the hash of the user's password...
+	const passwDigest = bcrypt.hashSync(req.body.password_digest, 10);
 
-  // This is where we obtain the hash of the user's password...
-  const passwDigest = bcrypt.hashSync(req.body.password_digest, 10);
+	// The req.body.counter should be set to 1 as an indicaton
+	// of active session... No re-login here...
 
-  // The req.body.counter should be set to 1 as an indicaton 
-  // of active session... No re-login here...
+	// Making sure that req.body.signedup_on is a valid
+	// timestamp...
+	const signOn = moment().unix();
 
-  // Making sure that req.body.signedup_on is a valid
-  // timestamp...
-  const signOn = moment().unix();
+	// The req.body.profiles_table field gets
+	// proper current value of "profiles"...
+	const profTable = 'profiles';
 
-  // The req.body.profiles_table field gets
-  // proper current value of "profiles"...
-  const profTable = 'profiles';
+	db
+		.one(
+			'INSERT INTO uesers (email, password_digest, counter, profiles_table) VALUES ($1, $2, $3, $4) RETURNING id;',
+			[req.body.email, passwDigest, 1, profTable]
+		)
+		.then(userId => {
+			res.locals.newUserId = userId;
+			console.log('pg-promise result data:', res.locals.newUserId);
 
-  db
-    .one(
-      'INSERT INTO uesers (email, password_digest, counter, signedup_on, profiles_table) VALUES ($1, $2, $3, $4, $5) RETURNING id;',
-      [
-        req.body.email,
-        passwDigest,
-        1,
-        signOn,
-        profTable
-
-      ]
-    )
-    .then(userId => {
-      res.locals.newUserId = userId;
-
-      console.log(res.locals.newUserId);
-
-      next();
-    })
-    .catch(error => {
-      // There supposed to be an error in case the email
-      // is not unique...
-      console.log('Error: in usersModel.create. Details: ', error);
-      next(error);
-    });
+			next();
+		})
+		.catch(error => {
+			// There supposed to be an error in case the email
+			// is not unique...
+			console.log('Error: in usersModel.create. Details: ', error);
+			next(error);
+		});
 };
 
 usersModel.getUser = (req, res, next) => {
+	console.log('in usersModel.getUser...');
 
-  console.log('in usersModel.getUser...');
+	const id = req.params.id;
+	console.log(`User's id is: ${id}...`);
 
-  const id = req.params.id;
-  console.log(`User's id is: ${id}...`);
-
-  db
-    .oneOrNone('SELECT * FROM users WHERE id = $1', [id])
-    .then(data => {
-      res.locals.user = data;
-      next();
-    })
-    // .catch(err => console.log(error));
-    .catch(error => {
-      console.log('Error: in usersModel.getUser. Details: ', error);
-      next(error);
-    });
+	db
+		.oneOrNone('SELECT * FROM users WHERE id = $1', [id])
+		.then(data => {
+			res.locals.user = data;
+			next();
+		})
+		// .catch(err => console.log(error));
+		.catch(error => {
+			console.log('Error: in usersModel.getUser. Details: ', error);
+			next(error);
+		});
 };
 
 // TODO: add method for updating user...
 
 usersModel.update = (req, res, next) => {
+	console.log('in usersModel.update...');
 
-  console.log('in usersModel.update...');
+	console.log('req.body:', req.body);
 
-  console.log('req.body:', req.body);
+	// We should not allow to update anything except:
+	// "profiles_table" field...
+	// Even changing the email is problematic...
+	// If a counter indicates an active session, there
+	// supposed to be forced logout/login with the new
+	// credentials... Delete user would be a better way...
+	let { email, password_digest, counter, signedup_on, profiles_table } = req.body;
 
-  // We should not allow to update anything except:
-  // "profiles_table" field... 
-  // Even changing the email is problematic... 
-  // If a counter indicates an active session, there
-  // supposed to be forced logout/login with the new 
-  // credentials... Delete user would be a better way...
-  let { email, password_digest, counter, signedup_on, profiles_table } = req.body;
+	db
+		.one('UPDATE users SET profiles_table=$1 WHERE id=$2 RETURNING id;', [
+			profiles_table,
+			req.params.id
+		])
+		.then(userId => {
+			res.locals.userId = userId;
+			next();
+		})
+		.catch(error => {
+			// There supposed to be an arror for non-unique email...
 
-  db
-    .one('UPDATE users SET profiles_table=$1 WHERE id=$2 RETURNING id;', 
-    [
-
-      profiles_table,
-      req.params.id
-
-    ])
-    .then(userId => {
-      res.locals.userId = userId;
-      next();
-    })
-    .catch(error => {
-
-      // There supposed to be an arror for non-unique email...
-      
-      console.log('Error: in usersModel.update. Details: ', error);
-      next(error);
-    });
+			console.log('Error: in usersModel.update. Details: ', error);
+			next(error);
+		});
 };
 
 // TODO: add method for deleting user
 
 usersModel.destroy = (req, res, next) => {
+	// What are the consiquencies of that?
+	// Is any user would be able to delete
+	// any other user?
 
-  // What are the consiquencies of that?
-  // Is any user would be able to delete
-  // any other user?
+	// On the list of all users it might be
+	// prevented by not allowing to destroy
+	// anyone except the current user...
 
-  // On the list of all users it might be 
-  // prevented by not allowing to destroy
-  // anyone except the current user...
+	console.log('in usersModel.destroy...');
 
-  console.log('in usersModel.destroy...');
-
-  db
-  .none('DELETE FROM users WHERE id=$1;', [req.params.id])
-  .catch(error => {
-    console.log('Error: in usersModel.destroy. Details: ', error);
-    next(error);
-  });
+	db.none('DELETE FROM users WHERE id=$1;', [req.params.id]).catch(error => {
+		console.log('Error: in usersModel.destroy. Details: ', error);
+		next(error);
+	});
 };
 
 module.exports = usersModel;
