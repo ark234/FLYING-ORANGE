e58279f0cac277496bbb65d4ca442a09f2f01aca
raksheen <raksheenayaz@gmail.com>
2018-02-20 18:31:40 -0500

    added search bar to results page

--- chewsy/client/src/components/Results.js
+++ chewsy/client/src/components/Results.js
@@ -1,60 +1,81 @@
 // import { Link } from "react-router-dom";
-import React, { Component } from 'react';
-import axios from 'axios';
-import '../App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "../App.css";
+import SearchField from "./SearchField";
 
 class ShowResults extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-		this.moreInfo = this.moreInfo.bind(this);
-	}
-	moreInfo(uri) {
-		axios({
-			url: 'http://localhost:8080/recipes/moreInfo',
-			method: 'post',
-			data: { uri }
-		}).then(response => {
-			console.log('POST FOR MORE INFO', response);
-			this.props.history.push('/moreInfo');
-		});
-	}
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.moreInfo = this.moreInfo.bind(this);
+    this.routeToResults = this.routeToResults.bind(this);
+  }
 
-	render() {
-		const results = this.props.results;
-		const resultsList = results.hits.map(recipeObject => {
-			const recipeInfo = recipeObject.recipe;
+  routeToResults() {
+    this.props.history.push("/results");
+  }
 
-			return (
-				<div key={recipeInfo.uri} className="recipeResultsContainer">
-					<div className="recipe-card">
-						<h3 className="viewRecipe">View</h3>
-						<div
-							onClick={() => {
-								this.props.moreInfo(recipeInfo);
-								this.props.history.push('/moreInfo');
-							}}
-						>
-							<img src={recipeInfo.image} className="recipeResultsImg" />
-							<h2 className="recipeName">{recipeInfo.label}</h2>
-							<h6 className="healthLabels">{recipeInfo.healthLabels}</h6>
-							<h6 className="servings">{recipeInfo.yield} servings</h6>
-							<h6 className="calories">{Math.trunc(recipeInfo.calories)} calories</h6>
-						</div>
-					</div>
-				</div>
-			);
-		});
+  moreInfo(uri) {
+    axios({
+      url: "http://localhost:8080/recipes/moreInfo",
+      method: "post",
+      data: { uri }
+    }).then(response => {
+      console.log("POST FOR MORE INFO", response);
+      this.props.history.push("/moreInfo");
+    });
+  }
 
-		return (
-			<div>
-				<div className="results-header">
-					Found {results.count} matching results for {results.q}
-				</div>
-				<div className="test">{resultsList}</div>
-			</div>
-		);
-	}
+  render() {
+    const results = this.props.results;
+    const resultsList = results.hits.map(recipeObject => {
+      const recipeInfo = recipeObject.recipe;
+
+      return (
+        <div>
+          <div key={recipeInfo.uri} className="recipeResultsContainer">
+            <div className="recipe-card">
+              <h3 className="viewRecipe">View</h3>
+              <div
+                onClick={() => {
+                  this.props.moreInfo(recipeInfo);
+                  this.props.history.push("/moreInfo");
+                }}
+              >
+                <img src={recipeInfo.image} className="recipeResultsImg" />
+                <h2 className="recipeName">{recipeInfo.label}</h2>
+                <h6 className="healthLabels">{recipeInfo.healthLabels}</h6>
+                <h6 className="servings">{recipeInfo.yield} servings</h6>
+                <h6 className="calories">
+                  {Math.trunc(recipeInfo.calories)} calories
+                </h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+
+    return (
+      <div>
+        <div className="search-bar">
+          <SearchField
+            routeToResults={this.routeToResults}
+            isLoaded={this.props.isLoaded}
+            errorForResponse={this.props.errorForResponse}
+            getResponseData={this.props.getResponseData}
+            errorFlag={this.props.errorFlag}
+            loadingFlag={this.props.loadingFlag}
+          />
+        </div>
+        <div className="results-header">
+          Found {results.count} matching results for {results.q}
+        </div>
+        <div className="test">{resultsList}</div>
+      </div>
+    );
+  }
 }
 
 export default ShowResults;
