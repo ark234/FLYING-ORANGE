abc2483f6cf24cad42cde7c0657bbd7fc4399c02
ark <ark234@gmail.com>
2018-02-19 20:15:56 -0500

    prettify

--- chewsy/client/src/components/Results.js
+++ chewsy/client/src/components/Results.js
@@ -1,50 +1,40 @@
-
 // import { Link } from "react-router-dom";
 import React, { Component } from 'react';
 import axios from 'axios';
 
-
 class ShowResults extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.moreInfo = this.moreInfo.bind(this);
-  }
-  moreInfo(uri) {
-    axios({
-      url: "http://localhost:8080/recipes/moreInfo",
-      method: "post",
-      data: uri
-    }).then(response => {
-      console.log("POST FOR MORE INFO", response);
-      this.props.history.push("/moreInfo");
-    });
-  }
+	constructor(props) {
+		super(props);
+		this.state = {};
+		this.moreInfo = this.moreInfo.bind(this);
+	}
+	moreInfo(uri) {
+		axios({
+			url: 'http://localhost:8080/recipes/moreInfo',
+			method: 'post',
+			data: uri
+		}).then(response => {
+			console.log('POST FOR MORE INFO', response);
+			this.props.history.push('/moreInfo');
+		});
+	}
 
-  
+	render() {
+		const results = this.props.results;
+		const resultsList = results.hits.map(recipeObject => {
+			const recipeInfo = recipeObject.recipe;
 
+			return (
+				<div key={recipeInfo.uri} onClick={() => this.moreInfo(recipeInfo.uri)}>
+					<h2>{recipeInfo.label}</h2>
+					<h6>{recipeInfo.healthLabels}</h6>
+					<img src={recipeInfo.image} width="100px" height="100px" />
+				</div>
+			);
+		});
 
-
-  render() {
-    const results = this.props.results;
-        const resultsList = results.hits.map(recipeObject => {
-          const recipeInfo = recipeObject.recipe;
-
-          return (
-            <div
-              key={recipeInfo.uri}
-              onClick={() => this.moreInfo(recipeInfo.uri)}
-            >
-              <h2>{recipeInfo.label}</h2>
-              <h6>{recipeInfo.healthLabels}</h6>
-              <img src={recipeInfo.image} width="100px" height="100px" />
-            </div>
-          )
-        })
-
-    return (<div>{resultsList}</div>);
-}
-
+		return <div>{resultsList}</div>;
+	}
 }
 
 export default ShowResults;
