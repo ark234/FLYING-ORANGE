a2d9334117533e73cf36b14c7d82b25380011ee0
ark <ark234@gmail.com>
2018-02-21 01:19:45 -0500

    finished building out backend for login/register with jwt auth. starting on front end

--- chewsy/client/src/App.js
+++ chewsy/client/src/App.js
@@ -1,17 +1,24 @@
-import React, { Component } from "react";
-import logo from "./images/orange.png";
-import "./App.css";
-import { BrowserRouter, Route, Link, Switch, Redirect } from "react-router-dom";
-import FixedNav from "./components/FixedNav";
-import HomeSearchForm from "./components/HomeSearchForm";
-import RecipeInfo from "./components/RecipeInfo";
-import Results from "./components/Results";
+import React, { Component } from 'react';
+import logo from './images/orange.png';
+import './App.css';
+import { BrowserRouter, Route, Link, Switch, Redirect } from 'react-router-dom';
+import FixedNav from './components/FixedNav';
+import HomeSearchForm from './components/HomeSearchForm';
+import RecipeInfo from './components/RecipeInfo';
+import Results from './components/Results';
+import TokenService from './services/TokenService';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { recipeData: null, isLoaded: null , moreInfo:[], signUpClicked: false, loginClicked: false};
+		this.state = {
+			recipeData: null,
+			isLoaded: null,
+			moreInfo: [],
+			signUpClicked: false,
+			loginClicked: false
+		};
 
 		this.getResponseData = this.getResponseData.bind(this);
 		this.errorForResponse = this.errorForResponse.bind(this);
@@ -19,23 +26,75 @@ class App extends Component {
 		this.getMoreInfoData = this.getMoreInfoData.bind(this);
 		this.toggleSignUp = this.toggleSignUp.bind(this);
 		this.toggleLogin = this.toggleLogin.bind(this);
+		this.register = this.register.bind(this);
+		this.login = this.login.bind(this);
+		this.logout = this.logout.bind(this);
 	}
 
-	toggleLogin(){
-		this.setState(prevState =>{
+	// api call for creating a new user
+	// note that TokenService.save with the token is called
+	// may also want to setState with the user data and
+	// whether or not the user is logged in
+	register(data) {
+		axios('http://localhost:3000/users/register', {
+			method: 'POST',
+			data
+		})
+			.then(resp => {
+				TokenService.save(resp.data.token);
+			})
+			.catch(err => console.log(`err: ${err}`));
+	}
+
+	// same as above except route is login
+	// as above, we are saving the token locally using
+	// the TokenService
+	login(data) {
+		axios('http://localhost:3000/users/login', {
+			method: 'POST',
+			data
+		})
+			.then(resp => {
+				TokenService.save(resp.data.token);
+			})
+			.catch(err => console.log(`err: ${err}`));
+	}
+
+	// calling a restricted route on the server
+	// the important part is setting the Authorization header
+	// with the token retrieved from the TokenService
+	authClick(ev) {
+		ev.preventDefault();
+		axios('http://localhost:3000/restricted', {
+			headers: {
+				Authorization: `Bearer ${TokenService.read()}`
+			}
+		})
+			.then(resp => console.log(resp))
+			.catch(err => console.log(err));
+	}
+
+	// just delete the token
+	logout(ev) {
+		ev.preventDefault();
+		TokenService.destroy();
+	}
+
+	toggleLogin() {
+		this.setState(prevState => {
 			prevState.loginClicked = !prevState.loginClicked;
 			return prevState;
 		});
 	}
-	toggleSignUp(){
-		this.setState(prevState =>{
+	toggleSignUp() {
+		this.setState(prevState => {
 			prevState.signUpClicked = !prevState.signUpClicked;
-			return prevState
-		})
+			return prevState;
+		});
 	}
 
 	getResponseData(responseData) {
-		console.log("grabbing data", responseData);
+		console.log('grabbing data', responseData);
 		this.setState({ recipeData: responseData, isLoaded: true });
 	}
 
@@ -80,7 +139,6 @@ class App extends Component {
 						path="/results"
 						render={props => {
 							return (
-
 								<Results
 									{...props}
 									results={this.state.recipeData}
@@ -91,7 +149,7 @@ class App extends Component {
 									loadingFlag={this.state.isLoaded}
 									isLoaded={this.loading}
 								/>
-							)
+							);
 						}}
 					/>
 					<Route
@@ -99,12 +157,19 @@ class App extends Component {
 						path="/moreInfo"
 						render={props => {
 							return (
-								<RecipeInfo 
-								toggleLogin={this.toggleLogin}
-								recipeDatum={this.state.moreInfo}
-								/>
-								)
+								<RecipeInfo toggleLogin={this.toggleLogin} recipeDatum={this.state.moreInfo} />
+							);
 						}}
+					/>
+					<Route
+						exact
+						path="/register"
+						component={props => <Register {...props} submit={this.register.bind(this)} />}
+					/>
+					<Route
+						exact
+						path="/login"
+						component={props => <Login {...props} submit={this.login.bind(this)} />}
 					/>
 				</Switch>
 			</BrowserRouter>
--- chewsy/client/src/components/Login.jsx
+++ chewsy/client/src/components/Login.jsx
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-class Login extends Component{
-  constructor(props){
-    super(props);
-    this.state = {};
-  }
-  render(){
-
-    return(
-      <div onClick={()=>this.props.toggleLogin()} className='modal'>
-
-      Login
-      </div>
-      )
-  }
-
+class Login extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		return (
+			<div onClick={() => this.props.toggleLogin()} className="modal">
+				Login
+			</div>
+		);
+	}
 }
-
-
 
 export default Login;
